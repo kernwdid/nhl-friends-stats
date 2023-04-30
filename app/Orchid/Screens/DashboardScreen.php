3c297d76449db5c2c5074d52d689669a788cca89
Didier Kernwein <didier@kernwein.ch>
2023-04-30 23:47:23 +0200

    more statistics

--- app/Orchid/Screens/DashboardScreen.php
+++ app/Orchid/Screens/DashboardScreen.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Orchid\Screens;
 
 use App\Models\Game;
+use Illuminate\Support\Facades\DB;
 use Orchid\Platform\Models\Role;
 use Orchid\Screen\Action;
 use Orchid\Screen\Screen;
@@ -37,25 +38,50 @@ class DashboardScreen extends Screen
                     ->where('home_user_id', $userId))
                 ->get();
 
-            foreach($usersPlayed as $player) {
-                    $wins = Game::where('home_user_id', $userId)
-                        ->where('away_user_id', $player->id)
-                        ->whereRaw('goals_home > goals_away')
-                        ->orWhere('away_user_id', $userId)
-                        ->where('home_user_id', $player->id)
-                        ->whereRaw('goals_home < goals_away')
-                        ->count();
+            foreach ($usersPlayed as $player) {
+                $wins = Game::where('home_user_id', $userId)
+                    ->where('away_user_id', $player->id)
+                    ->whereRaw('goals_home > goals_away')
+                    ->orWhere('away_user_id', $userId)
+                    ->where('home_user_id', $player->id)
+                    ->whereRaw('goals_home < goals_away')
+                    ->count();
 
-                    $losses = Game::where('home_user_id', $userId)
-                        ->where('away_user_id', $player->id)
-                        ->whereRaw('goals_home < goals_away')
-                        ->orWhere('away_user_id', $userId)
-                        ->where('home_user_id', $player->id)
-                        ->whereRaw('goals_home > goals_away')
-                        ->count();
+                $highestWin = Game::where('home_user_id', $userId)
+                    ->where('away_user_id', $player->id)
+                    ->whereRaw('goals_home > goals_away')
+                    ->select(DB::raw('(games.goals_home AS first, games.goals_away AS second, games.goals_home - games.goals_away) AS highest_win'))
+                    ->orWhere('away_user_id', $userId)
+                    ->where('home_user_id', $player->id)
+                    ->whereRaw('goals_home < goals_away')
+                    ->select(DB::raw('games.goals_home AS second, games.goals_away AS first, games.goals_away - games.goals_home AS highest_win'))
+                    ->orderBy('highest_win', 'desc')
+                    ->first();
 
-                    $metrics['wins_' . $player['id']] = ['value' => number_format($wins)];
-                    $metrics['losses_' . $player['id']] = ['value' => number_format($losses)];
+                $losses = Game::where('home_user_id', $userId)
+                    ->where('away_user_id', $player->id)
+                    ->whereRaw('goals_home < goals_away')
+                    ->orWhere('away_user_id', $userId)
+                    ->where('home_user_id', $player->id)
+                    ->whereRaw('goals_home > goals_away')
+                    ->count();
+
+                $highestLoss = Game::where('home_user_id', $userId)
+                    ->where('away_user_id', $player->id)
+                    ->whereRaw('goals_home < goals_away')
+                    ->select(DB::raw('games.goals_home AS first, games.goals_away AS second, (games.goals_away - games.goals_home) AS highest_loss'))
+                    ->orWhere('away_user_id', $userId)
+                    ->where('home_user_id', $player->id)
+                    ->whereRaw('goals_home > goals_away')
+                    ->select(DB::raw('games.goals_home AS second, games.goals_away AS first, games.goals_home - games.goals_away AS highest_loss'))
+                    ->orderBy('highest_loss', 'asc')
+                    ->first();
+
+                $metrics['wins_' . $player['id']] = ['value' => number_format($wins)];
+                $metrics['losses_' . $player['id']] = ['value' => number_format($losses)];
+                $metrics['highest_win_' . $player['id']] = ['value' => $highestWin['first'] . " - " . $highestWin['second']];
+                $metrics['highest_loss_' . $player['id']] = ['value' => $highestLoss['first'] . " - " . $highestLoss['second']];
+
             }
 
             $query['metrics'] = $metrics;
@@ -123,10 +149,12 @@ class DashboardScreen extends Screen
                     ->where('home_user_id', $userId))
                 ->get();
 
-            foreach($usersPlayed as $player) {
+            foreach ($usersPlayed as $player) {
                 $name = $player['name'];
                 $metrics[__('dashboard.wins_against') . " " . $name] = 'metrics.wins_' . $player['id'];
                 $metrics[__('dashboard.losses_against') . " " . $name] = 'metrics.losses_' . $player['id'];
+                $metrics[__('dashboard.highest_win_against') . " " . $name] = 'metrics.highest_win_' . $player['id'];
+                $metrics[__('dashboard.highest_loss_against') . " " . $name] = 'metrics.highest_loss_' . $player['id'];
             }
 
             $content[] = Layout::metrics($metrics);
--- lang/de/dashboard.php
+++ lang/de/dashboard.php
@@ -3,5 +3,7 @@
 return [
     'description' => 'Hier können die neusten Statistiken und Auswertungen genossen werden.',
     'wins_against' => 'Siege vs',
-    'losses_against' => 'Niederlagen vs'
+    'losses_against' => 'Niederlagen vs',
+    'highest_win_against' => 'Höchster Sieg vs',
+    'highest_loss_against' => 'Höchste Niederlage vs'
 ];
